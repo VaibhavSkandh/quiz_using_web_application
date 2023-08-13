41f289c44821ed4b3000d49d116ade906a401f76
VaibhavSkandh <132656970+VaibhavSkandh@users.noreply.github.com>
2023-08-13 22:43:01 +0530

    Add files via upload

---
+++ quiz.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Website</title>
+    <link rel="stylesheet" href="quiz.css">
+</head>
+<body>
+   
+    <header>
+        <div class="container" >
+            <p style="text-align:center;font-weight:bolder;font-family:monospace;font-size:xx-large">PHP Quizer</p>
+        </div>
+    </header>
+    <main>
+        <div class="menu">
+            <br><br>
+
+            <a href="main.php" class="button">Take Quiz</a>
+            <!-- this button takes to main.php file -->
+       
+
+            <a href="add.php" class="button">Add Questions</a>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <h2 style="font-family:monospace">W E L C O M E</h2>
+        </div>
+    </footer>
+    
+</body>
+</html>
